117c9b7db58569cc2e729b689fe459f8b4d5eca7
Lewis J. Hannah <lewis.j.hannah@gmail.com>
2025-05-18 07:13:25 -0400

    Add Swagger Support

--- TitleTrackAPI/Program.cs
+++ TitleTrackAPI/Program.cs
@@ -23,9 +23,18 @@ builder.Services.AddScoped<ITitleAbstractRepository, TitleAbstractRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 app.UseCors("MyCors");
 
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TitleTrackAPI v1"));
+}
 
 app.Run();
